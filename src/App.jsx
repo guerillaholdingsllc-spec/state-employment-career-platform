7adbe6b3a300155ca04814e1a110075ee92a9c46
Guerilla Holdings, LLC <guerillaholdingsllc@gmail.com>
2026-07-10 11:14:16 -0700

    Create App component with header and description
    Initial implementation of the App component with a header and description.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+    return (
+          <div className="App">
+                <header className="App-header">
+                        <h1>State Civil Service Career Platform</h1>h1>
+                        <p>Centralized job recruitment and career management system</p>p>
+                </header>header>
+          </div>div>
+        );
+}
+
+export default App;</div>
